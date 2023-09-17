9e72be78151fc393bfa38a475fa7bd3137862b91
Julian <julikq22@gmail.com>
2023-09-17 15:30:26 +0200

    First commit

---
+++ cypress/e2e/tests/test1.cy.js
@@ -0,0 +1,6 @@
+describe('Primeros tests de prueba', () =>{
+    beforeEach('hacemos test de prueba luego cambiamos descripción', () =>{
+        cy.visit('https://juliako-trials710.orangehrmlive.com/client/#/dashboard')
+    })
+
+})
